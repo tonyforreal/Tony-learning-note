5536ed265439fd8152ed008a7a1249797aa0d5b1
tonyforreal <55642973+tonyforreal@users.noreply.github.com>
2019-10-10 20:43:49 +0800

    Create SortAnArray.py

---
+++ LeetCode/SortAnArray.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+      if len(nums) < 2:
+          return nums
+      else:
+          pivot = nums[0]
+          less = [i for i in nums[1:] if i <= pivot]
+          greater = [i for i in nums[1:] if i > pivot]
+          return self.sortArray(less) + [pivot] + self.sortArray(greater)
