6f051d3526dbf23c12f4dad42d03567cdf54a87d
tonyforreal <55642973+tonyforreal@users.noreply.github.com>
2019-10-10 22:39:22 +0800

    Update QuickSort.py

--- Homework/QuickSort.py
+++ Homework/QuickSort.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 def quicksort(x):
     if len(x) < 2:
         return x
@@ -12,30 +6,3 @@ def quicksort(x):
         less=[i for i in x[1:] if i <= pivot]
         more=[i for i in x[1:] if i > pivot]
         return quicksort(less)+[pivot]+quicksort(more)
-
-
-# In[3]:
-
-
-#如果數列中的數字不到兩個就不用跑else
-#統一用數列的第一個數作為pivot，比pivot小的放less[]裡，大的放more[]裡
-#將新的數列重整，若less[]與more[]中還有兩個或以上的數字，則重複分類一次，直到所有list中都只剩下一個數，就會停止分類
-
-
-# In[4]:
-
-
-Alist=[4,7,3,0,9,8,5,1,6,2]
-
-
-# In[5]:
-
-
-quicksort(Alist)
-
-
-# In[ ]:
-
-
-
-
