e4a3632dc13e689910158248d72929a73ff206f4
tonyforreal <55642973+tonyforreal@users.noreply.github.com>
2019-10-18 16:09:03 +0800

    Delete QuickSort.py

--- Homework/QuickSort.py
+++
@@ -1,8 +0,0 @@
-def quicksort(x):
-    if len(x) < 2:
-        return x
-    else:
-        pivot=x[0]
-        less=[i for i in x[1:] if i <= pivot]
-        more=[i for i in x[1:] if i > pivot]
-        return quicksort(less)+[pivot]+quicksort(more)
