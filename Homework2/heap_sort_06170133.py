21580f49d6d5f99889c95ee669e7a2dd708693c6
tonyforreal <55642973+tonyforreal@users.noreply.github.com>
2019-11-01 22:51:02 +0800

    Update heap_sort_06170133.py

--- Homework2/heap_sort_06170133.py
+++ Homework2/heap_sort_06170133.py
@@ -2,3 +2,7 @@
 
 class Solution(object):
     def heap_sort(self, nums):
+        """
+        :type nums: List[int] ex:[3,2,-4,6,4,2,19],[5,1,1,2,0,0]
+        :rtype: List[int] ex:[-4,2,2,3,4,6,19],[0,0,1,1,2,5]
+        """
