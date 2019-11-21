1b72779be832bdae647b54707dafa89ade23abae
tonyforreal <55642973+tonyforreal@users.noreply.github.com>
2019-11-21 18:38:42 +0800

    Create binary_search_tree_06170133.py

---
+++ HW3/binary_search_tree_06170133.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self,x):
+        self.val = x
+        self.left = None
+        self.right = None
+        """
+        :type val: int
+        :type left: TreeNode or None
+        :type right: TreeNode or None
+        """
+class Solution(object):
+    def insert(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode(inserted node)
+        """
+    def delete(self, root, target):
+        """
+        :type root: TreeNode
+        :type target: int
+        :rtype: TreeNode(the root of new completed binary search tree) (cannot search())
+        """
+    def search(self, root, target):
+        """
+        :type root: TreeNode
+        :type target: int
+        :rtype: TreeNode(searched node)
+        """
+    def modify(self, root, target, new_val):
+        """
+        :type root: TreeNode
+        :type target: int
+        :type new_val: int
+        :rtype:TreeNode(the root of new completed binary search tree) (cannot search())
+        """
