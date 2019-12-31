724750acf4fbe11ce3515288ab740d7a0c7a2207
tonyforreal <55642973+tonyforreal@users.noreply.github.com>
2019-12-31 19:19:46 +0800

    Update LinkedList.py

--- LeetCode/LinkedList.py
+++ LeetCode/LinkedList.py
@@ -5,7 +5,7 @@ class MyLinkedList:
         Initialize your data structure here.
         """
         self.val = None
-        self.next = None
+        self.next=None
 
     def get(self, index: int) -> int:
         """
@@ -13,95 +13,97 @@ class MyLinkedList:
         """
         if self.val == None:
             return -1
-        if index == 0:
-            return self.val
-        p = self.next
-        i = 1
-        while p != None:
+        j=self
+        i=0
+        while j!=None:
             if i == index:
-                return p.val
-            p = p.next
-            i += 1
+                return j.val
+            j=j.next
+            i+=1
         return -1
+
+        
 
     def addAtHead(self, val: int) -> None:
         """
         Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
         """
-        if self.val == None:
-            self.val = val
-            return
-        temp = self.val
-        self.val = val
-        tempnode = self.next
-        self.next = MyLinkedList()
-        self.next.val = temp
-        self.next.next = tempnode
-        return
-
+        if self.val==None:
+            self.val=val
+        else:
+            j=self.val
+            n=self.next
+            self.val=val
+            self.next=MyLinkedList()
+            self.next.val=j
+            self.next.next=n
+            
+        
     def addAtTail(self, val: int) -> None:
         """
         Append a node of value val to the last element of the linked list.
         """
-        if self.val == None:
-            self.val = val
-            return
-        p = self
-        while p.next != None:
-            p = p.next
-        p.next = MyLinkedList()
-        p.next.val = val
-        return
+        if self.val==None:
+            self.val=val
+        elif self.val!=None:
+            j=self
+            i=0
+            while j.next!=None:
+                j=j.next
+                i+=1
+            j.next=MyLinkedList()
+            j.next.val=val
+        
+        
+        
 
-
-    def addAtIndex(self, index: int, val: int) -> None:
+    def addAtIndex(self, index: int, val: int) -> None:#在第幾項插入值
         """
         Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
         """
-        i = 0
-        p = self
-        pre = p
-        if index <= 0:
-            self.addAtHead(val)
-            return
-        while i < index:
-            i += 1
-            pre = p
-            if p != None and p.val != None:
-                p = p.next
+        i=0
+        t=0
+        j=self
+        index=index-1
+        while i<index:
+            i+=1
+            if j.next!=None:
+                j=j.next
+                n=j
             else:
-                return
-        pre.next = MyLinkedList()
-        pre.next.val = val
-        pre.next.next = p
-        return
+                t+=1
+        if index<=-1:
+            self.addAtHead(val)
+        elif t==0 and j.next!=None:
+            n=j.next
+            j.next=MyLinkedList()
+            j.next.val=val
+            j.next.next=n
+        elif t<=1 and j.val!=None:
+            self.addAtTail(val)
             
 
     def deleteAtIndex(self, index: int) -> None:
         """
         Delete the index-th node in the linked list, if the index is valid.
         """
-        i = 0
-        p = self
-        if index < 0:
-            return
-        if index == 0:
-            if self.val == None:
-                return
-            if self.next == None:
-                self = None
-                return
-            self.val = self.next.val
-            self.next = self.next.next
-        pre = p
-        while i < index:
-            i += 1
-            pre = p
-            if pre == None:
-                return
-            p = p.next
-        if p != None:
-            pre.next = p.next
+        i=0
+        j=self
+        t=0
+        while i<index:
+            i+=1
+            if j.next!=None:
+                n=j
+                j=j.next
+            else:
+                t+=1
+        if index==0 and j.next==None:
+            j.val=None
+        elif index>=0 and t==0 and j.next!=None:
+            j.val=j.next.val
+            j.next=j.next.next
+        elif index>0 and t==0 and j.val!=None:
+            n.next=None
         else:
-            pre.next = None
-        return 
+            return
+
