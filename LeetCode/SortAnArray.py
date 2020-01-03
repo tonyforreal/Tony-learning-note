523694f74fb47657791f108e88f58a755d4389f0
tonyforreal <55642973+tonyforreal@users.noreply.github.com>
2020-01-03 23:08:38 +0800

    Delete SortAnArray.py

--- LeetCode/SortAnArray.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def sortArray(self, nums: List[int]) -> List[int]:
-      if len(nums) < 2:
-          return nums
-      else:
-          pivot = nums[0]
-          less = [i for i in nums[1:] if i <= pivot]
-          more = [i for i in nums[1:] if i > pivot]
-          return self.sortArray(less) + [pivot] + self.sortArray(more)
