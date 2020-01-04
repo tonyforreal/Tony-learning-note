72cd353ee4c67066e0f058cff7d03c91b150eaed
tonyforreal <55642973+tonyforreal@users.noreply.github.com>
2020-01-04 13:28:06 +0800

    Update Dijkstra_06170133.py

--- HW6/Dijkstra_06170133.py
+++ HW6/Dijkstra_06170133.py
@@ -13,29 +13,66 @@ class Graph():
     def addEdge(self,u,v,w): 
         self.dict[w].append([u,v])
     
-    def Dijkstra(self, s): 
-        test = [False] * self.V 
-        distance = [float('inf')] * self.V
-        
-        distance[s] = 0
-        
-        while test != [True] * self.V:
-            for i in range(self.V):
-                if g.graph[s][i] != 0 and distance[s] + g.graph[s][i] < distance[i]:
-                    distance[i] = distance[s] + g.graph[s][i]
-                           
-            m = float('inf')
-            for k in range(self.V):
-                if test[k] == False and distance[k] < m :
-                    m = distance[k]
-                    s = k         
-            test[s] = True
+    from collections import defaultdict 
+
+
+class Graph(): 
+
+    def __init__(self, vertices): 
+        self.V = vertices 
+        self.graph = [] 
+        self.graph_matrix = [[0 for column in range(vertices)]  
+                    for row in range(vertices)] 
+        self.dict = defaultdict(list) 
+    
+    def addEdge(self,u,v,w): 
+        self.dict[w].append([u,v])
+    
+    def Dijkstra(self, s):
+        import math
+
+        values = [math.inf]*self.V
+        values[s] = 0
+        visited =  set()
+        unvisited = set([x for x in range(self.V)])
+        curr = s
+        while curr is not None: 
             
-            answer = {} 
-            if test == [True] * self.V:
-                for i in range(self.V):
-                    answer[str(i)] = distance[i]
-                return answer
+            neighbors = self.graph[curr]
+            for i, x in enumerate(neighbors):
+                if x == 0 or i in visited: continue
+                values[i] = min(values[i], values[curr] + x)
+
+            visited.add(curr)
+            unvisited.remove(curr)
+            
+            next_curr = None
+            min_val = max(values)
+            
+            if not unvisited: break
+            for node in unvisited:
+                if values[node] < min_val:
+                    next_curr = node
+                    min_val = values[node]
+            curr = next_curr
+           
+        ret = {str(i):x for i, x in enumerate(values)}
+        return ret
+    
+    def Kruskal(self):
+        answer={}
+        check = []
+        for i in range(self.V):
+            check.append(i)
+        val = sorted(self.dict)
+        for i in val:
+            for f,s in self.dict[i]:
+                if check[f] == check[s]:
+                    pass
+                else:
+                    check = [check[f] if x==check[s] else x for x in check]
+                    answer[str(f)+'-'+str(s)] = i
+        return answer
     
     def Kruskal(self):
         answer={}
